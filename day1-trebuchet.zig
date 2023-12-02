f7365b69f6f4e18d2a2838f6e71d71ce0a831852
Juan Llamas <38849891+xoltia@users.noreply.github.com>
2023-12-02 01:12:53 -0600

    Day 1

---
+++ day1-trebuchet.zig
@@ -0,0 +1,92 @@
+const std = @import("std");
+
+fn getDigitValueFromWordWindow(window: []const u8, fromBack: bool) ?usize {
+    const words = [10][]const u8{
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    };
+
+    if (fromBack) {
+        for (words, 0..) |word, i| {
+            var windowMax = if (word.len > window.len) window.len else word.len;
+
+            if (std.mem.eql(u8, word, window[window.len - windowMax ..])) {
+                return i;
+            }
+        }
+
+        return null;
+    }
+
+    for (words, 0..) |word, i| {
+        var windowMax = if (word.len > window.len) window.len else word.len;
+
+        if (std.mem.eql(u8, word, window[0..windowMax])) {
+            return i;
+        }
+    }
+
+    return null;
+}
+
+pub fn main() !void {
+    var file = try std.fs.cwd().openFile("input.txt", .{});
+    defer file.close();
+
+    var allocator = std.heap.page_allocator;
+    var reader = file.reader();
+
+    var total: usize = 0;
+
+    while (try reader.readUntilDelimiterOrEofAlloc(allocator, '\n', 1024)) |line| {
+        for (line, 0..) |c, i| {
+            switch (c) {
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' => {
+                    total += (c - '0') * 10;
+                    break;
+                },
+                else => {
+                    var digit = getDigitValueFromWordWindow(line[i..], false);
+                    if (digit != null) {
+                        total += digit.? * 10;
+                        break;
+                    }
+                },
+            }
+        }
+
+        var i = line.len - 1;
+
+        while (i > 0) : (i -= 1) {
+            switch (line[i]) {
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' => {
+                    total += (line[i] - '0');
+                    break;
+                },
+                else => {
+                    var digit = getDigitValueFromWordWindow(line[0 .. i + 1], true);
+                    if (digit != null) {
+                        total += digit.?;
+                        break;
+                    }
+                },
+            }
+        }
+
+        if (i == 0) {
+            total += (line[i] - '0');
+        }
+
+        allocator.free(line);
+    }
+
+    std.debug.print("Total: {}\n", .{total});
+}
