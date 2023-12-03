2fb5796653e880976b83c531ae06968d7be0015e
Juan Llamas <38849891+xoltia@users.noreply.github.com>
2023-12-03 13:50:56 -0600

    Day3 part2

--- day-03-gear-ratios.zig
+++ day-03-gear-ratios.zig
@@ -1,5 +1,38 @@
 const std = @import("std");
 
+const ProductList = struct {
+    x: usize,
+    y: usize,
+    value: u32,
+    n: usize,
+
+    fn multiply(self: *const ProductList, num: u32) ProductList {
+        return .{ .x = self.x, .y = self.y, .value = self.value * num, .n = self.n + 1 };
+    }
+};
+
+fn multiplyByCoord(list: *std.ArrayList(ProductList), p: ProductList) std.mem.Allocator.Error!void {
+    std.debug.print("multiplyByCoord({d}, {d}, {d})\n", .{ p.x, p.y, p.value });
+    for (list.items, 0..) |item, i| {
+        std.debug.print("\tProductList{{ .x = {d}, .y = {d}, .value = {d} }}\n", .{ item.x, item.y, item.value });
+        if (item.x == p.x and item.y == p.y) {
+            list.items[i] = item.multiply(p.value);
+            std.debug.print(" => {d}\n", .{item.value});
+            return;
+        }
+    }
+    return list.append(p);
+}
+
+fn sumList(list: std.ArrayList(ProductList)) u32 {
+    var sum: u32 = 0;
+    for (list.items) |item| {
+        if (item.n == 0) continue;
+        sum += item.value;
+    }
+    return sum;
+}
+
 pub fn main() !void {
     var reader = std.io.getStdIn().reader();
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -17,6 +50,9 @@ pub fn main() !void {
     var y: usize = 0;
     var nextRow = rowIterator.next();
     var prevRow: ?[]const u8 = null;
+
+    var productList = std.ArrayList(ProductList).init(allocator);
+    defer productList.deinit();
 
     while (y < n) : (y += 1) {
         const row = nextRow.?;
@@ -64,10 +100,38 @@ pub fn main() !void {
 
                 if (adjacent) sum += number;
             }
+
+            if (x < row.len and row[x] == '*') {
+                try multiplyByCoord(&productList, .{ x, y, number, 0 });
+            }
+            if ((lowerX != 0) and row[lowerX - 1] == '*') {
+                try multiplyByCoord(&productList, .{ lowerX - 1, y, number, 0 });
+            }
+
+            const safeUpperX = @min(x + 1, row.len);
+            const safeLowerX = if (lowerX == 0) 0 else (lowerX - 1);
+
+            if (prevRow) |r| {
+                for (r[safeLowerX..safeUpperX], 0..) |ca, i| {
+                    if (ca == '*') {
+                        try multiplyByCoord(&productList, .{ safeLowerX + i, y - 1, number, 0 });
+                    }
+                }
+            }
+
+            if (nextRow) |r| {
+                for (r[safeLowerX..safeUpperX], 0..) |ca, i| {
+                    if (ca == '*') {
+                        try multiplyByCoord(&productList, .{ safeLowerX + i, y + 1, number, 0 });
+                    }
+                }
+            }
         }
 
         prevRow = row;
     }
 
-    std.debug.print("{any}\n", .{sum});
+    const productSum = sumList(productList);
+
+    std.debug.print("{any}\n{any}\n", .{ sum, productSum });
 }
