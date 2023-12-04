16d2281d6303199d95d941a4dcf7dfae83b35290
Juan Llamas <38849891+xoltia@users.noreply.github.com>
2023-12-04 12:46:31 -0600

    Day 4 part 1 optimize

--- day-04-scratchcards.zig
+++ day-04-scratchcards.zig
@@ -3,39 +3,23 @@ const std = @import("std");
 const number_iterator = struct {
     input: []const u8,
 
-    fn collect(self: *number_iterator, allocator: std.mem.Allocator) !std.ArrayList(*const [2]u8) {
-        var list = try std
-            .ArrayList(*const [2]u8)
-            .initCapacity(allocator, self.remaining());
-
-        while (self.next()) |digits| {
-            try list.append(digits);
-        }
-
-        return list;
-    }
-
     fn next(self: *number_iterator) ?*const [2]u8 {
-        if (self.input.len < 4) {
+        if (self.input.len < 3) {
             return null;
         }
 
         const digits = self.input[1..3];
-        // var result: u8 = if (digits[0] != ' ') (digits[0] - '0') * 10 else 0;
-        // result += digits[1] - '0';
         self.input = self.input[3..];
         return digits;
     }
 
     fn remaining(self: number_iterator) usize {
-        return (self.input.len - 1) / 3;
+        return self.input.len / 3;
     }
 };
 
 pub fn main() !void {
-    const allocator = std.heap.page_allocator;
-
-    var args = try std.process.argsWithAllocator(allocator);
+    var args = std.process.args();
 
     const program_name = args.next().?;
     const file_name = args.next();
@@ -46,31 +30,36 @@ pub fn main() !void {
     }
 
     const file = try std.fs.cwd().openFile(file_name.?, .{});
-    const reader = file.reader();
+    var reader = file.reader();
 
-    const offset = 9;
+    var buff: [1024 * 100]u8 = undefined;
+    var n = try reader.readAll(&buff);
+
+    var lines = std.mem.splitScalar(u8, buff[0..n], '\n');
+    const offset = std.mem.indexOfScalar(u8, buff[0..n], ':').? + 1;
+
     var total_points: usize = 0;
 
-    while (try reader.readUntilDelimiterOrEofAlloc(allocator, '\n', 1024)) |line| {
-        defer allocator.free(line);
-
+    while (lines.next()) |line| {
         var lists = std.mem.splitScalar(u8, line[offset..], '|');
-        const winning_list_input = lists.next() orelse unreachable;
 
-        var winning_iterator = number_iterator{ .input = winning_list_input };
-        const winning_list = try winning_iterator.collect(allocator);
-        defer winning_list.deinit();
+        var winning_list_input = lists.next() orelse unreachable;
+        winning_list_input = winning_list_input[0 .. winning_list_input.len - 1];
 
         const owned_list_input = lists.next() orelse unreachable;
+
         var owned_iterator = number_iterator{ .input = owned_list_input };
+        var winning_iterator = number_iterator{ .input = winning_list_input };
         var i: u6 = 0;
 
         while (owned_iterator.next()) |num| {
-            for (winning_list.items) |num2| {
+            while (winning_iterator.next()) |num2| {
                 if (std.mem.eql(u8, num, num2)) {
                     i += 1;
                 }
             }
+
+            winning_iterator = number_iterator{ .input = winning_list_input };
         }
 
         total_points += if (i == 0) 0 else @as(u64, 1) << (i - 1);
